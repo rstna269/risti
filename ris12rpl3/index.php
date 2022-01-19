3b3a9c6ebdd775816805a4663fd429fa79e8b1e0
rstna269 <93703270+rstna269@users.noreply.github.com>
2022-01-20 05:16:32 +0700

    Add files via upload
    new

---
+++ ris12rpl3/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    include("konten/head.html");
+    ?>
+</head>
+<body>
+    <div class="container">
+        <?php
+        include("konten/judul.html");
+        include("konten/nav.php");
+        ?>
+
+    </div>
+    
+</body>
+</html>
